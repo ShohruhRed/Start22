01dbe4acddd60aa265c9a465a51286699e343160
Shohruh Abduvohidov <shohruhred@inbox.ru>
2021-06-22 19:53:46 +0500

    Notifypropertychanged

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Start22.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
